58ab64f72ae33535a0b56707e738ef63ab3974f5
Frankx772 <simonfr@onet.pl>
2020-12-09 01:29:21 +0100

    modyfikacje

--- board-js.js
+++ board-js.js
@@ -26,15 +26,12 @@ document.getElementById("submit").onclick = show_cards;
 fetch('https://deckofcardsapi.com/api/deck/new/')
   .then(response => response.json())
   .then(data => {
+      console.log(data);
          decks_id = data.deck_id;
-        startCards();
-    }) 
+    }).then(drawFromDeck)
 
 
-function startCards()
-{
-    drawFromDeck();
-}
+
 //drawing all cards from deck
  function drawFromDeck()
 {
@@ -44,7 +41,7 @@ function startCards()
         {
             console.log(data);
         }).then(addToPile)
-        .then(shuffle_piles)
+        
 
     
 }
@@ -56,7 +53,7 @@ async function addToPile()
     
     //add to pile 1
     //AS,2S,3S,4S,5S,6S,7S,8S,9S,0S,JS,QS,KS,AD,2D,3D,4D,5D,6D,7D,8D,9D,0D,JD,QD,KD (SPADES AND DIAMONDS)
-    fetch('https://deckofcardsapi.com/api/deck/' + decks_id + '/pile/player1/add/?cards=AS,2S,3S,4S,5S,6S,7S,8S,9S,0S,JS,QS,KS,AD,2D,3D,4D,5D,6D,7D,8D,9D,0D,JD,QD,KD')
+   await fetch('https://deckofcardsapi.com/api/deck/' + decks_id + '/pile/player1/add/?cards=AS,2S,3S,4S,5S,6S,7S,8S,9S,0S,JS,QS,KS,AD,2D,3D,4D,5D,6D,7D,8D,9D,0D,JD,QD,KD')
     .then(response => response.json())
     .then(data =>
     { 
@@ -67,7 +64,7 @@ async function addToPile()
     
     
 
-    fetch('https://deckofcardsapi.com/api/deck/' + decks_id + '/pile/player2/add/?cards=AH,2H,3H,4H,5H,6H,7H,8H,9H,0H,JH,QH,KH,AC,2C,3C,4C,5C,6C,7C,8C,9C,0C,JC,QC,KC')
+   await fetch('https://deckofcardsapi.com/api/deck/' + decks_id + '/pile/player2/add/?cards=AH,2H,3H,4H,5H,6H,7H,8H,9H,0H,JH,QH,KH,AC,2C,3C,4C,5C,6C,7C,8C,9C,0C,JC,QC,KC')
     .then(response => response.json())
     .then(data =>{ 
         addToPile2_success = data.success;
@@ -75,7 +72,7 @@ async function addToPile()
     })
     
     
-
+    shuffle_piles();
 }
 
 
@@ -83,39 +80,34 @@ async function addToPile()
  async function shuffle_piles()
 {
     
-    fetch('https://deckofcardsapi.com/api/deck/' + decks_id + '/pile/player2/shuffle/')
+  await  fetch('https://deckofcardsapi.com/api/deck/' + decks_id + '/pile/player2/shuffle/')
     .then(response => response.json())
     .then(data =>{ 
         draw1_success = data.success;
         console.log('shuffle1');
     })
-    //.then(check_draw)
+    
     
 
-    fetch('https://deckofcardsapi.com/api/deck/' + decks_id + '/pile/player1/shuffle/')
+   await fetch('https://deckofcardsapi.com/api/deck/' + decks_id + '/pile/player1/shuffle/')
     .then(response => response.json())
     .then(data =>{ 
         draw2_success = data.success;
         console.log('shuffle2');
     })
-    //.then(check_draw)
-    
-    async function check_draw()
-    {
-if(addToPile1_success == true & addToPile2_success == true)
-    {
-    /////
 
-    }
+
+    await drawFromPile();
+
     }
 
     //drawFromPile();
-}
 
-function drawFromPile()
+
+async function drawFromPile()
 {
-    console.log("2");
-    fetch('https://deckofcardsapi.com/api/deck/' + decks_id + '/pile/player1/draw/')
+    console.log("1");
+    await fetch('https://deckofcardsapi.com/api/deck/' + decks_id + '/pile/player1/draw/')
     .then(response => response.json())
     .then(data =>
         {
@@ -123,9 +115,9 @@ function drawFromPile()
              played_cards_code = data.cards[0].code;
             played_cards_value = data.cards[0].value;
             played_cards_img = data.cards[0].image;
-        }).then(convert);
+        }).then(convert)
 
-        fetch('https://deckofcardsapi.com/api/deck/' + decks_id + '/pile/player2/draw/')
+        await fetch('https://deckofcardsapi.com/api/deck/' + decks_id + '/pile/player2/draw/')
         .then(response => response.json())
         .then(data =>
             {
@@ -135,8 +127,8 @@ function drawFromPile()
                 si_cards_img = data.cards[0].image;
                 
 
-            })
-            .then(convert);
+            }).then(convert)
+            
 
         function convert()
             {
@@ -187,48 +179,45 @@ function drawFromPile()
 
 
     }
-  function show_cards()
-{
-    addPoint().then(drawFromPile)
-   //newCard().then(war())
-
-
-}
-
-async function addPoint()
-{
-    console.log("1");
-    document.getElementById("player_card").src = played_cards_img;
-    document.getElementById("si_card").src = si_cards_img;
-     console.log(played_cards_value + ' player');
-     console.log(si_cards_value + ' si');
  
-     
-     
-     if(played_cards_value > si_cards_value)
-     {   
-         document.getElementById("player-win").innerHTML = "Wygrana!";
-         document.getElementById("si-win").innerHTML = "";
-         player_points++;
-         document.getElementById("player-score").innerHTML = player_points;
- 
-     }else if(played_cards_value < si_cards_value){
-         document.getElementById("si-win").innerHTML = "Wygrana!";
-         document.getElementById("player-win").innerHTML = "";
-         si_points++;
-         document.getElementById("si-score").innerHTML = si_points;
-     }else if(played_cards_value == si_cards_value){
-         war();
-     }
-}
-async function newCard()
+async function show_cards()
 {
-    console.log("2");
+    
+     function add_point()
+        {
+        document.getElementById("player_card").src = played_cards_img;
+        document.getElementById("si_card").src = si_cards_img;
+        console.log(played_cards_value + ' player');
+        console.log(si_cards_value + ' si');
+    
+        
+        
+        if(played_cards_value > si_cards_value)
+        {   
+            document.getElementById("player-win").innerHTML = "Wygrana!";
+            document.getElementById("si-win").innerHTML = "";
+            player_points++;
+            document.getElementById("player-score").innerHTML = player_points;
+    
+        }else if(played_cards_value < si_cards_value){
+            document.getElementById("si-win").innerHTML = "Wygrana!";
+            document.getElementById("player-win").innerHTML = "";
+            si_points++;
+            document.getElementById("si-score").innerHTML = si_points;
+        }else if(played_cards_value == si_cards_value){
+            war();
+        }
+        }
 
-    console.log('essa');
-   drawFromPile();
+        await add_point()
+        await drawFromPile();
 }
+
+
 function war()
 {
+    document.getElementById("si-win").innerHTML = "";
+    document.getElementById("player-win").innerHTML = "";
+    
     console.log("war!");
 }
