7f5baa37084f986823e35111af1ff3a3fd04fd44
Frankx772 <simonfr@onet.pl>
2020-12-17 02:43:52 +0100

    dodanie ekarnu końca gry

--- board-js.js
+++ board-js.js
@@ -2,7 +2,7 @@
 var name = sessionStorage.getItem("name");
 document.getElementById("name").innerHTML = name;
 var decks_id = "";
-var player_points = 0;
+var player_points = 1;
 var si_points = 0;
 var played_cards_code = "";
 var played_cards_value = "";
@@ -18,6 +18,7 @@ document.getElementById("player-score").innerHTML = player_points;
 document.getElementById("si-score").innerHTML = si_points;
 document.getElementById("submit").onclick = show_cards;
 
+endgame();
 
 
 //new deck //
@@ -109,15 +110,16 @@ async function drawFromPile()
     .then(data =>
         {
             console.log(data);
+            draw_success1 = data.success;
              played_cards_code = data.cards[0].code;
             played_cards_value = data.cards[0].value;
             played_cards_img = data.cards[0].image;
-            draw_success1 = data.success;
+            
 
-            if(draw_success1 == false)
-            {
-                endgame();
-            }
+           
+        })
+        .catch((error) => {
+            endgame();
         }).then(convert)
 
         await fetch('https://deckofcardsapi.com/api/deck/' + decks_id + '/pile/player2/draw/')
@@ -125,17 +127,18 @@ async function drawFromPile()
         .then(data =>
             {
                 console.log(data);
+                draw_success2 = data.success;
+                console.log(draw_success1);
                  si_cards_code = data.cards[0].code;
                 si_cards_value = data.cards[0].value;
                 si_cards_img = data.cards[0].image;
-                draw_success2 = data.success;
-                console.log(draw_success1);
-                if(draw_success2 == false)
-                {
-                    endgame();
-                }
+
+          
                 
 
+            })
+            .catch((error) => {
+                endgame();
             }).then(convert)
             
 
@@ -163,22 +166,22 @@ async function drawFromPile()
                 }
 
                 //convert values to si cards
-                if(si_cards_value === 'ACE')
+                if(si_cards_value == 'ACE')
                 {
                     si_cards_value = '1';
                 }
 
-                if(si_cards_value === 'JACK')
+                if(si_cards_value == 'JACK')
                 {
                     si_cards_value = '11';
                 }
 
-                if(si_cards_value === 'QUEEN')
+                if(si_cards_value == 'QUEEN')
                 {
                     si_cards_value = '12';
                 }
 
-                if(si_cards_value === 'KING')
+                if(si_cards_value == 'KING')
                 {
                     si_cards_value = '13';
                 }
@@ -208,8 +211,8 @@ async function drawFromPile()
             document.getElementById("si-win").innerHTML = "";
             player_points++;
             document.getElementById("player-score").innerHTML = player_points;
-            
             drawFromPile();
+
                
         }else if(played_cards_value < si_cards_value){
             document.getElementById("si-win").innerHTML = "Wygrana!";
@@ -244,15 +247,90 @@ function war()
 
 function endgame()
 {
+    var winner = "";
+    
     var body = document.body;
     var board = document.getElementById("board");
+    board.style.display = "none";
     var end_game_div = document.createElement("div");
     var end_game_div_element = document.createElement("div");
-    body.style.backgroundColor = "#FFE4C4";
+    end_game_div.style.backgroundColor = "#FFE4C4";
     end_game_div.className = "end_game";
     end_game_div_element.className = "end_game_element";
+    var end_game_text_container = document.createElement("div");
+    end_game_text_container.className = "end_game_text_container";
+    var text_container1 = document.createElement("div");
+    text_container1.className = "podsumowanie";
+    var text_container2 = document.createElement("div");
+    text_container2.className = "text_container";
+    var text_container3 = document.createElement("div");
+    text_container3.className = "text_container";
+    var text_container4 = document.createElement("div");
+    text_container4.className = "textS_container";
+    var text_container5 = document.createElement("div");
+    text_container5.className = "textS_container";
+
+    var endOfGame = document.createTextNode("KONIEC GRY");
+    var text1 = document.createTextNode("Podsumowanie");
+    var text2 = document.createTextNode(name);
+    var text3 = document.createTextNode("Si");
+    var text4 = document.createTextNode(player_points);
+    var text5 = document.createTextNode(si_points)
 
     body.appendChild(end_game_div);
     end_game_div.appendChild(end_game_div_element);
-    board.style.display = "none";
+    end_game_div_element.appendChild(end_game_text_container);
+    end_game_div_element.appendChild(text_container1);
+    end_game_div_element.appendChild(text_container2);
+    end_game_div_element.appendChild(text_container3);
+    end_game_div_element.appendChild(text_container4);
+    end_game_div_element.appendChild(text_container5);
+
+    end_game_text_container.appendChild(endOfGame);
+    text_container1.appendChild(text1);
+    text_container2.appendChild(text2);
+    text_container3.appendChild(text3);
+    text_container4.appendChild(text4);
+    text_container5.appendChild(text5);
+
+    var werdykt = document.createElement("div");
+    werdykt.className = "werdykt";
+    if(player_points>si_points){
+        winner = "Zwycięża " +name;
+    }else if(si_points>player_points){
+        winner = "Zwycięża Si";
+    }else if(si_points == player_points){
+        winner = "Remis";
+    }
+
+    var werdykt_text = document.createTextNode("Wynik: " + winner)
+    end_game_div_element.appendChild(werdykt);
+    werdykt.appendChild(werdykt_text);
+
+    //restart button
+    var button = document.createElement("div");
+    button.className = "restart";
+
+    end_game_div_element.appendChild(button);
+
+    var button_text = document.createTextNode("Zagraj jeszcze raz");
+    button.appendChild(button_text);
+
+ //   button.onclick = window.location.reload();
+//name change button
+
+    var nameChangeButton = document.createElement("div");
+    nameChangeButton.className = "nameChange";
+
+    end_game_div_element.appendChild(nameChangeButton);
+
+    var nameChange_text = document.createTextNode("Zmień nick");
+    nameChangeButton.appendChild(nameChange_text);
+
+   // nameChangeButton.onclick = changeLocation();
+}
+
+function changeLocation()
+{
+    location.href = "index.html"
 }
